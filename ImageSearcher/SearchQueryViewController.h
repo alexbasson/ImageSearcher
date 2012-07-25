d421400f1b693222e9555c20af34d3dcd232a4d8
Alex Basson <alexbasson@gmail.com>
2012-07-25 16:28:07 -0400

    Added SearchQueryViewController class.

---
+++ ImageSearcher/SearchQueryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchQueryViewController.h
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchQueryViewController : UIViewController
+
+@end
---
+++ ImageSearcher/SearchQueryViewController.m
@@ -0,0 +1,44 @@
+//
+//  SearchQueryViewController.m
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import "SearchQueryViewController.h"
+
+@interface SearchQueryViewController ()
+
+@end
+
+@implementation SearchQueryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
