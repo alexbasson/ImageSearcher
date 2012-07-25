544956162062c336537fbdc0d51a73780705c4d3
Alex Basson <alexbasson@gmail.com>
2012-07-25 16:25:32 -0400

    Created GoogleImage class, added properties to it.

---
+++ ImageSearcher/GoogleImage.h
@@ -0,0 +1,29 @@
+//
+//  GoogleImage.h
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleImage : NSObject
+
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSString *contentNoFormatting;
+@property (strong, nonatomic) NSNumber *height;
+@property (strong, nonatomic) NSStream *html;
+@property (strong, nonatomic) NSURL *originalContextURL;
+@property (strong, nonatomic) NSNumber *tbHeight;
+@property (strong, nonatomic) NSURL *tbUrl;
+@property (strong, nonatomic) NSNumber *tbWidth;
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *titleNoFormatting;
+@property (strong, nonatomic) NSURL *unescapedUrl;
+@property (strong, nonatomic) NSURL *url;
+@property (strong, nonatomic) NSURL *visibleUrl;
+@property (strong, nonatomic) NSNumber *width;
+
+
+@end
---
+++ ImageSearcher/GoogleImage.m
@@ -0,0 +1,29 @@
+//
+//  GoogleImage.m
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import "GoogleImage.h"
+
+@implementation GoogleImage
+
+@synthesize content = _content;
+@synthesize contentNoFormatting = _contentNoFormatting;
+@synthesize height = _height;
+@synthesize html = _html;
+@synthesize originalContextURL = _originalContextURL;
+@synthesize tbHeight = _tbHeight;
+@synthesize tbUrl = _tbUrl;
+@synthesize tbWidth = _tbWidth;
+@synthesize title = _title;
+@synthesize titleNoFormatting = _titleNoFormatting;
+@synthesize unescapedUrl = _unescapedUrl;
+@synthesize url = _url;
+@synthesize visibleUrl = _visibleUrl;
+@synthesize width = _width;
+
+
+@end
