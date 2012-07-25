eff818a7b60ec987e69d2f045304b546f23f635c
Alex Basson <alexbasson@gmail.com>
2012-07-25 16:23:59 -0400

    Initial Commit

---
+++ ImageSearcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageSearcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageSearcher
+//
+//  Created by Alex Basson on 7/25/12.
+//  Copyright (c) 2012 Poly Prep C.D.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
